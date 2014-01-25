14d97d3b573f5d0fee1bbddd1d5567f896630f70
tonypes <tonypesonen@gmail.com>
2014-01-25 15:27:24 +0200

    Finish sing-in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,12 @@ module ApplicationHelper
     else
       "#{base_title} | #{page_title}"
     end
+end
+
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
   end
 end
